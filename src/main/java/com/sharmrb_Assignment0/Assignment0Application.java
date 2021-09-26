f3f6b5986518fa19ad32d92298cb9a1b127f02bc
sharmrb <57459468+sharmrb@users.noreply.github.com>
2021-09-26 14:00:41 -0400

    Creating the springboot template

---
+++ src/main/java/com/sharmrb_Assignment0/Assignment0Application.java
@@ -0,0 +1,13 @@
+package com.sharmrb_Assignment0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharmrb_Assignment0/Assignment0/Assignment0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharmrb_Assignment0.Assignment0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
